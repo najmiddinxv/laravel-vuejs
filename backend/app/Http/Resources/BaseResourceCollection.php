c9db89545d18acdd8b67039899df71a04a1a4db3
Najmiddin <129720773+bekdevz@users.noreply.github.com>
2024-05-21 19:23:46 +0500

    updated

--- backend/app/Http/Resources/BaseResourceCollection.php
+++ backend/app/Http/Resources/BaseResourceCollection.php
@@ -23,6 +23,51 @@ class BaseResourceCollection extends ResourceCollection
             'last_page' => $resource->lastPage(),
             'prev_page_url' => $resource->currentPage() > 1 ? $resource->path()."?page=".$resource->currentPage()-1 : null,
                 
+            // {
+                
+            //     "links": {
+            //         "first": "http://127.0.0.1:8000/api/v1/posts?page=1",
+            //         "last": "http://127.0.0.1:8000/api/v1/posts?page=3",
+            //         "prev": null,
+            //         "next": "http://127.0.0.1:8000/api/v1/posts?page=2"
+            //     },
+            //     "meta": {
+            //         "current_page": 1,
+            //         "from": 1,
+            //         "last_page": 3,
+            //         "links": [
+            //             {
+            //                 "url": null,
+            //                 "label": "&laquo; Previous",
+            //                 "active": false
+            //             },
+            //             {
+            //                 "url": "http://127.0.0.1:8000/api/v1/posts?page=1",
+            //                 "label": "1",
+            //                 "active": true
+            //             },
+            //             {
+            //                 "url": "http://127.0.0.1:8000/api/v1/posts?page=2",
+            //                 "label": "2",
+            //                 "active": false
+            //             },
+            //             {
+            //                 "url": "http://127.0.0.1:8000/api/v1/posts?page=3",
+            //                 "label": "3",
+            //                 "active": false
+            //             },
+            //             {
+            //                 "url": "http://127.0.0.1:8000/api/v1/posts?page=2",
+            //                 "label": "Next &raquo;",
+            //                 "active": false
+            //             }
+            //         ],
+            //         "path": "http://127.0.0.1:8000/api/v1/posts",
+            //         "per_page": 2,
+            //         "to": 2,
+            //         "total": 6
+            //     }
+            // }
         ];
 
         $resource = $resource->getCollection(); // Necessary to remove meta and links
